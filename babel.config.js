2a15c38af7488816da0355b6450b3589e1f50f18
Ko Nyan <extnyanlin.tun@7peakssoftware.com>
2023-03-26 10:44:05 +0700

    add paper lib and jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@core': './src/core',
+            '@assets': './assets',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@svgIcons': './src/svgIcons',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+      '@babel/plugin-proposal-export-namespace-from',
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };

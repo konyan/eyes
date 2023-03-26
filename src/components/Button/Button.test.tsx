100de02b97cf21a515523986d079296bc6da4a4f
Ko Nyan <extnyanlin.tun@7peakssoftware.com>
2023-03-26 14:44:47 +0700

    lint

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render, fireEvent } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import Button from './Button';
 
 const mockPress = jest.fn();

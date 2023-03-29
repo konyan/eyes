a6b13f2ad98286f547a1a852c9fc541bdc96b718
Kaung Htet Naing <11kaungkaung99@gmail.com>
2023-03-29 22:25:08 +0630

    add test feat/GradientButton

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Button/Button';
 export { default as ChipButton } from './ChipButton/ChipButton';
+export { default as GradientButton } from './GradientButton/GradientButton';

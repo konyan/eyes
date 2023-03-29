2e3cd9c24a70083b01c13e81d7762b7dc323b6d5
Ko Nyan <devkonyan@gmail.com>
2023-03-29 22:10:56 +0700

    fix and update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Button/Button';
 export { default as ChipButton } from './ChipButton/ChipButton';
+export { default as SearchInput } from './SearchInput/SearchInput';

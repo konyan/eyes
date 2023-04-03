571981d33d6889c2abdced42c863a7f83936cb8e
Kaung Htet Naing <11kaungkaung99@gmail.com>
2023-04-03 16:34:02 +0630

    add sketch

--- src/screens/LoadingWheelScreen/LoadingWheelScreen.tsx
+++ src/screens/LoadingWheelScreen/LoadingWheelScreen.tsx
@@ -1,40 +1,62 @@
 import Fontisto from '@expo/vector-icons/Fontisto';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import React, { useContext, useState } from 'react';
-import { Animated, Image, SafeAreaView, StatusBar, Text, TouchableOpacity, View } from 'react-native';
-import { ThemeContext } from 'src/context/ThemeContext/ThemeContext';
+import React, { useContext, useMemo, useState } from 'react';
+import { Animated, Image, Platform, SafeAreaView, StatusBar, Text, TouchableOpacity, View } from 'react-native';
+import { GradientButton } from 'src/components';
 import tw from 'twrnc';
+import { ThemeContext } from '../../context/ThemeContext/ThemeContext';
 
-const SpinningWheel = () => {
+const SpinningWheel = ({ navigation }) => {
   const [angle, setAngle] = useState(0);
   const { themeValue, toggleTheme, getTheme } = useContext(ThemeContext);
   const [isClicked, setIsClicked] = useState(false);
-  const opacity = useState(new Animated.Value(1))[0];
+  // const opacity = useState(new Animated.Value(1))[0];
+  const opacity = useMemo(() => new Animated.Value(1), []);
+  const scale = useMemo(() => new Animated.Value(1), []);
 
-  const rotate = (value) => {
+  const injectWebStyles = Platform.OS === 'web' ? 'w-full h-1/2' : 'w-max h-1/2';
+
+  const rotate = (value: number) => {
     setAngle(value);
   };
 
   const handleClick = () => {
+    startAnimation();
     setIsClicked(true);
-    Animated.timing(opacity, {
-      toValue: 0,
-      duration: 500,
-      useNativeDriver: true,
-    }).start(() => {
-      setTimeout(() => {
-        setIsClicked(false);
-        Animated.timing(opacity, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: true,
-        }).start();
-      }, 2000);
+    // Animated.timing(opacity, {
+    // toValue: 0,
+    // duration: 2000,
+    // useNativeDriver: true,
+    // }).start(() => {
+    // setIsClicked(false);
+    // Animated.timing(opacity, {
+    // toValue: 1,
+    // duration: 2000,
+    // useNativeDriver: true,
+    // }).start();
+    // });
+    Animated.parallel([
+      Animated.timing(scale, {
+        toValue: 0,
+        duration: 3000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 3000,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      setIsClicked(false);
+      scale.setValue(1);
+      opacity.setValue(1);
     });
+
+    // navigation.navigate('ResultScreen');
   };
 
   const startAnimation = () => {
-    const duration = 2; // in seconds
+    const duration = 2;
     const fps = 60;
     const frames = duration * fps;
     const anglePerFrame = 720 / frames;
@@ -44,6 +66,7 @@ const SpinningWheel = () => {
 
     const animation = setInterval(() => {
       angleAnimate += anglePerFrame;
+
       rotate(angleAnimate);
       currentFrame++;
       if (currentFrame === frames) {
@@ -53,7 +76,7 @@ const SpinningWheel = () => {
   };
 
   return (
-    <SafeAreaView style={[tw`flex-1 justify-center items-center`, { marginTop: StatusBar.currentHeight }]}>
+    <SafeAreaView style={[tw`flex-1 items-center`, { marginTop: StatusBar.currentHeight }]}>
       <View style={tw`h-1/6  flex-row items-center justify-center `}>
         <Text style={tw`text-2xl font-bold p-1 `}>နတ်မျက်စိ ဗေဒင်</Text>
         <TouchableOpacity activeOpacity={1} onPress={toggleTheme}>
@@ -66,25 +89,17 @@ const SpinningWheel = () => {
       </View>
       <Image
         source={require('../../../assets/splash.png')}
-        style={{ width: 400, height: 400, transform: [{ rotate: `${angle}deg` }] }}
+        resizeMode="contain"
+        style={[tw`${injectWebStyles}`, { transform: [{ rotate: `${angle}deg` }] }]}
       />
-      <Text>ယုံကြည်စွာဖြင့်ဆုတောင်းရန် နှိပ်ပါ</Text>
-      <TouchableOpacity onPress={startAnimation}>
-        <Text>Spin the Wheel!</Text>
-      </TouchableOpacity>
-      <View>
-        {isClicked ? (
-          <Animated.View style={{ opacity }}>
-            <TouchableOpacity style={{ backgroundColor: 'blue', padding: 20 }} disabled={true}>
-              <Text style={{ color: 'white', fontWeight: 'bold' }}>Clicked!</Text>
-            </TouchableOpacity>
-          </Animated.View>
-        ) : (
-          <TouchableOpacity onPress={handleClick} style={{ backgroundColor: 'blue', padding: 20 }}>
-            <Text style={{ color: 'white', fontWeight: 'bold' }}>Click Me!</Text>
-          </TouchableOpacity>
-        )}
-      </View>
+      <Text style={tw`text-xl font-black tracking-widest my-10`}>ယုံကြည်စွာဖြင့်ဆုတောင်းရန် နှိပ်ပါ</Text>
+      {isClicked ? (
+        <Animated.View style={{ transform: [{ scale }], opacity }}>
+          <GradientButton onPress={handleClick} text="ဗေဒင်မေးမည်" />
+        </Animated.View>
+      ) : (
+        <GradientButton onPress={handleClick} text="ဗေဒင်မေးမည်" />
+      )}
     </SafeAreaView>
   );
 };

e057a69fa17b9263768928a8f346eeab68b3c11e
Ko Nyan <devkonyan@gmail.com>
2023-04-14 15:53:43 +0700

    setup env for firebase

--- src/core/firebaseAuth.tsx
+++ src/core/firebaseAuth.tsx
@@ -1,16 +1,28 @@
+import Constants from 'expo-constants';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 // Initialize Firebase
 // Your web app's Firebase configuration
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} = Constants?.expoConfig?.extra || {};
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyB-6oixNjHvqihoPCo0i7v5ONXhvtNqQWk',
-  authDomain: 'donation-887a7.firebaseapp.com',
-  databaseURL: 'https://donation-887a7.firebaseio.com',
-  projectId: 'donation-887a7',
-  storageBucket: 'donation-887a7.appspot.com',
-  messagingSenderId: '606079671431',
-  appId: '1:606079671431:web:68b4348d8f2b9f3d6dac22',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
